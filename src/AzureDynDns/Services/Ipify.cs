8a82ee9b76122930b9495fe4f3efb226ccb4e1b7
Restyled.io <commits@restyled.io>
2020-02-20 22:01:48 +0000

    Restyled by astyle

--- src/AzureDynDns/Services/Ipify.cs
+++ src/AzureDynDns/Services/Ipify.cs
@@ -7,48 +7,48 @@ using System.Threading.Tasks;
 
 namespace AzureDynDns.Services
 {
-    public class Ipify : IIpify
+public class Ipify : IIpify
+{
+    private readonly IHttpClientFactory clientFactory;
+    private readonly ILogger<IIpify> logger;
+    private readonly Uri serviceUri;
+
+    public Ipify(
+        IpifyConfiguration config,
+        IHttpClientFactory httpFactory,
+        ILogger<IIpify> logger)
     {
-        private readonly IHttpClientFactory clientFactory;
-        private readonly ILogger<IIpify> logger;
-        private readonly Uri serviceUri;
-
-        public Ipify(
-            IpifyConfiguration config,
-            IHttpClientFactory httpFactory,
-            ILogger<IIpify> logger)
+        // Use default public service if not specified otherwise
+        if (string.IsNullOrWhiteSpace(config.IpifyServiceAddress))
         {
-            // Use default public service if not specified otherwise
-            if (string.IsNullOrWhiteSpace(config.IpifyServiceAddress))
-            {
-                config.IpifyServiceAddress = "https://api.ipify.org";
-            }
-
-            serviceUri = new Uri(config.IpifyServiceAddress);
-            clientFactory = httpFactory;
-            this.logger = logger;
+            config.IpifyServiceAddress = "https://api.ipify.org";
         }
 
-        public async Task<string> GetPublicIP()
+        serviceUri = new Uri(config.IpifyServiceAddress);
+        clientFactory = httpFactory;
+        this.logger = logger;
+    }
+
+    public async Task<string> GetPublicIP()
+    {
+        using (var client = clientFactory.CreateClient())
         {
-            using (var client = clientFactory.CreateClient())
+            using (HttpResponseMessage response = await client.GetAsync(serviceUri).ConfigureAwait(false))
             {
-                using (HttpResponseMessage response = await client.GetAsync(serviceUri).ConfigureAwait(false))
+                if (response.IsSuccessStatusCode)
                 {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        var ip = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                        logger.LogInformation("Retrieved public IP {publicIP}", ip);
-                        return ip;
-                    }
-                    else
-                    {
-                        string errorMessage = await response.SafeReadStringContentsAsync().ConfigureAwait(false);
-                        logger.LogError("Failed to invoke {apiUrl}. Status code {statusCode}. Error message {errorMessage}", serviceUri, response.StatusCode, errorMessage);
-                        return null;
-                    }
+                    var ip = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    logger.LogInformation("Retrieved public IP {publicIP}", ip);
+                    return ip;
+                }
+                else
+                {
+                    string errorMessage = await response.SafeReadStringContentsAsync().ConfigureAwait(false);
+                    logger.LogError("Failed to invoke {apiUrl}. Status code {statusCode}. Error message {errorMessage}", serviceUri, response.StatusCode, errorMessage);
+                    return null;
                 }
             }
         }
     }
+}
 }
