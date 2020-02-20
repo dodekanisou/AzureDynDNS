9ddf124c145638972cdf5ab42b7ca89d490196c6
Restyled.io <commits@restyled.io>
2020-02-20 21:51:09 +0000

    Restyled by clang-format

--- src/AzureDynDns/LibExtensions.cs
+++ src/AzureDynDns/LibExtensions.cs
@@ -8,57 +8,56 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AzureDynDns
-{
-public static class LibExtensions
-{
-    public static IServiceCollection RegisterServices(this IServiceCollection services, IConfiguration configuration)
-    {
-        // Register the AzureDnsConfiguration object
-        AzureDnsConfiguration azureDnsConfig = new AzureDnsConfiguration();
-        configuration.Bind("Settings", azureDnsConfig);
-        services.AddSingleton(azureDnsConfig);
+namespace AzureDynDns {
+public static class LibExtensions {
+  public static IServiceCollection
+  RegisterServices(this IServiceCollection services,
+                   IConfiguration configuration) {
+    // Register the AzureDnsConfiguration object
+    AzureDnsConfiguration azureDnsConfig = new AzureDnsConfiguration();
+    configuration.Bind("Settings", azureDnsConfig);
+    services.AddSingleton(azureDnsConfig);
 
-        // Register the IpifyConfiguration object
-        IpifyConfiguration ipifyConfig = new IpifyConfiguration();
-        configuration.Bind("Settings", ipifyConfig);
-        services.AddSingleton(ipifyConfig);
+    // Register the IpifyConfiguration object
+    IpifyConfiguration ipifyConfig = new IpifyConfiguration();
+    configuration.Bind("Settings", ipifyConfig);
+    services.AddSingleton(ipifyConfig);
 
-        // Register the AzureDynDnsConfiguration object
-        AzureDynDnsConfiguration azureDynDnsConfig = new AzureDynDnsConfiguration();
-        configuration.Bind("Settings", azureDynDnsConfig);
-        services.AddSingleton(azureDynDnsConfig);
+    // Register the AzureDynDnsConfiguration object
+    AzureDynDnsConfiguration azureDynDnsConfig = new AzureDynDnsConfiguration();
+    configuration.Bind("Settings", azureDynDnsConfig);
+    services.AddSingleton(azureDynDnsConfig);
 
-        // Register the default IHttpClientFactory
-        services.AddHttpClient();
+    // Register the default IHttpClientFactory
+    services.AddHttpClient();
 
-        // Register application services
-        services.AddSingleton<IIpify, Ipify>();
-        services.AddSingleton<IAzureDnsService, AzureDnsService>();
-        services.AddSingleton<IAzureDynDnsService, AzureDynDnsService>();
+    // Register application services
+    services.AddSingleton<IIpify, Ipify>();
+    services.AddSingleton<IAzureDnsService, AzureDnsService>();
+    services.AddSingleton<IAzureDynDnsService, AzureDynDnsService>();
 
-        return services;
+    return services;
+  }
+
+  [System.Diagnostics.CodeAnalysis.SuppressMessage(
+      "Design", "CA1031:Do not catch general exception types",
+      Justification = "Unsure of what exceptions may be thrown at this point")]
+  public static async Task<string>
+  SafeReadStringContentsAsync(this HttpResponseMessage responseMessage) {
+    string content = string.Empty;
+    if (responseMessage == null) {
+      return content;
     }
 
-    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1031:Do not catch general exception types", Justification = "Unsure of what exceptions may be thrown at this point")]
-    public static async Task<string> SafeReadStringContentsAsync(this HttpResponseMessage responseMessage)
-    {
-        string content = string.Empty;
-        if (responseMessage == null)
-        {
-            return content;
-        }
-
-        try
-        {
-            content = await responseMessage.Content.ReadAsStringAsync().ConfigureAwait(false);
-        }
-        catch (Exception)
-        {
-            // Ignore failure to read response
-        }
-
-        return content;
+    try {
+      content =
+          await responseMessage.Content.ReadAsStringAsync().ConfigureAwait(
+              false);
+    } catch (Exception) {
+      // Ignore failure to read response
     }
+
+    return content;
+  }
 }
 }
