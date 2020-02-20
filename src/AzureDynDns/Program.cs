ba22b19736ad5afa16792174b18b0a77cc01d7ab
Restyled.io <commits@restyled.io>
2020-02-20 22:01:48 +0000

    Restyled by astyle

--- src/AzureDynDns/Program.cs
+++ src/AzureDynDns/Program.cs
@@ -7,34 +7,34 @@ using System.Reflection;
 
 namespace AzureDynDns
 {
-    public static class Program
+public static class Program
+{
+    public static void Main()
     {
-        public static void Main()
+        var assembly = Assembly.GetEntryAssembly();
+
+        // Create configuration object
+        IConfiguration config = new ConfigurationBuilder()
+        .SetBasePath(Environment.CurrentDirectory)
+        .AddJsonFile("appsettings.json", true, true)
+        .AddUserSecrets(assembly)
+        .Build();
+
+        // Setup our DI
+        var serviceProvider = new ServiceCollection()
+        .AddLogging()
+        .AddSingleton<IConfiguration>(config)
+        .RegisterServices(config)
+        .AddLogging((builder) =>
         {
-            var assembly = Assembly.GetEntryAssembly();
+            builder.AddConsole();
+            builder.SetMinimumLevel(LogLevel.Trace);
+        })
+        .BuildServiceProvider();
 
-            // Create configuration object
-            IConfiguration config = new ConfigurationBuilder()
-                                        .SetBasePath(Environment.CurrentDirectory)
-                                        .AddJsonFile("appsettings.json", true, true)
-                                        .AddUserSecrets(assembly)
-                                        .Build();
-
-            // Setup our DI
-            var serviceProvider = new ServiceCollection()
-                                      .AddLogging()
-                                      .AddSingleton<IConfiguration>(config)
-                                      .RegisterServices(config)
-                                      .AddLogging((builder) =>
-                                      {
-                                          builder.AddConsole();
-                                          builder.SetMinimumLevel(LogLevel.Trace);
-                                      })
-                                      .BuildServiceProvider();
-
-            var updateService = serviceProvider.GetService<IAzureDynDnsService>();
-            var ip = updateService.UpdateDynamicDnsRecord().GetAwaiter().GetResult();
-            Console.WriteLine($"Assigned IP {ip}");
-        }
+        var updateService = serviceProvider.GetService<IAzureDynDnsService>();
+        var ip = updateService.UpdateDynamicDnsRecord().GetAwaiter().GetResult();
+        Console.WriteLine($"Assigned IP {ip}");
     }
+}
 }
