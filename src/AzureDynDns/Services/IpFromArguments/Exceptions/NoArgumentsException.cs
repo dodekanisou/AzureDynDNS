3ea84e006e5d1eb873b2e511ed4cac40fb2abdb4
Restyled.io <commits@restyled.io>
2020-06-06 11:22:51 +0000

    Restyled by astyle

--- src/AzureDynDns/Services/IpFromArguments/Exceptions/NoArgumentsException.cs
+++ src/AzureDynDns/Services/IpFromArguments/Exceptions/NoArgumentsException.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace AzureDynDns.Services.IpFromArguments.Exceptions {
-  public class NoArgumentsException : Exception {
+public class NoArgumentsException : Exception {
     public NoArgumentsException() : base("Arguments: No arguments found.") {}
-  }
+}
 }
