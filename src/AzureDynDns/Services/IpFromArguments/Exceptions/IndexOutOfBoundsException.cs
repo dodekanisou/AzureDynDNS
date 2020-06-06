708a458223fb8238cafdbb91c838dc30f95d3e32
Restyled.io <commits@restyled.io>
2020-06-06 14:14:40 +0000

    Restyled by astyle

--- src/AzureDynDns/Services/IpFromArguments/Exceptions/IndexOutOfBoundsException.cs
+++ src/AzureDynDns/Services/IpFromArguments/Exceptions/IndexOutOfBoundsException.cs
@@ -1,24 +1,24 @@
 ﻿using System;
 
 namespace AzureDynDns.Services.IpFromArguments.Exceptions {
-  public class IndexOutOfBoundsException : ArgumentOutOfRangeException {
+public class IndexOutOfBoundsException : ArgumentOutOfRangeException {
     public IndexOutOfBoundsException(IpFromArgumentsConfiguration config)
         : base($"ArgumentIndex: Didn't pass enough arguments. " +
                $"Asked for index {config.ArgumentIndex} and got " +
                $"{config.Arguments.Count} arguments." +
                $"TIP: Index starts from zero not one.") {
-      this.RequestedIndex = config.ArgumentIndex;
-      this.ElementsCount = config.Arguments.Count;
+        this.RequestedIndex = config.ArgumentIndex;
+        this.ElementsCount = config.Arguments.Count;
     }
 
     public int RequestedIndex {
-      get;
-      set;
+        get;
+        set;
     }
 
     public int ElementsCount {
-      get;
-      set;
+        get;
+        set;
     }
-  }
+}
 }
