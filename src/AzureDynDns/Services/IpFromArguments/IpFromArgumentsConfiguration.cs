6618bef1791a2716f275b1b60eba7954b84b808e
Restyled.io <commits@restyled.io>
2020-06-06 11:17:11 +0000

    Restyled by astyle

--- src/AzureDynDns/Services/IpFromArguments/IpFromArgumentsConfiguration.cs
+++ src/AzureDynDns/Services/IpFromArguments/IpFromArgumentsConfiguration.cs
@@ -4,16 +4,22 @@ using System.Text;
 
 namespace AzureDynDns.Services.IpFromArguments
 {
-    public class IpFromArgumentsConfiguration
-    {
-        /// <summary>
-        /// The index of the argument to use as the IP.
-        /// </summary>
-        public int ArgumentIndex { get; set; }
-
-        /// <summary>
-        /// The arguments passed to the application.
-        /// </summary>
-        public IList<string> Arguments { get; set; }
+public class IpFromArgumentsConfiguration
+{
+    /// <summary>
+    /// The index of the argument to use as the IP.
+    /// </summary>
+    public int ArgumentIndex {
+        get;
+        set;
     }
+
+    /// <summary>
+    /// The arguments passed to the application.
+    /// </summary>
+    public IList<string> Arguments {
+        get;
+        set;
+    }
+}
 }
