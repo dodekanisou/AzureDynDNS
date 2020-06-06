baf32ae753b7f10b7db4a566d2c600f504ecaa95
Restyled.io <commits@restyled.io>
2020-06-06 14:14:40 +0000

    Restyled by clang-format

--- src/AzureDynDns/Program.cs
+++ src/AzureDynDns/Program.cs
@@ -7,30 +7,30 @@ using System.Reflection;
 
 namespace AzureDynDns {
 public static class Program {
-    public static void Main(string[] args) {
-        var assembly = Assembly.GetEntryAssembly();
+  public static void Main(string[] args) {
+    var assembly = Assembly.GetEntryAssembly();
 
-        // Create configuration object
-        IConfiguration config = new ConfigurationBuilder()
-        .SetBasePath(assembly.AssemblyDirectory())
-        .AddJsonFile("appsettings.json", true, true)
-        .AddUserSecrets(assembly)
-        .Build();
+    // Create configuration object
+    IConfiguration config = new ConfigurationBuilder()
+                                .SetBasePath(assembly.AssemblyDirectory())
+                                .AddJsonFile("appsettings.json", true, true)
+                                .AddUserSecrets(assembly)
+                                .Build();
 
-        // Setup our DI
-        var serviceProvider = new ServiceCollection()
-        .AddLogging()
-        .AddSingleton<IConfiguration>(config)
-        .RegisterServices(config, args)
-        .AddLogging((builder) => {
-            builder.AddConsole();
-            builder.SetMinimumLevel(LogLevel.Trace);
-        })
-        .BuildServiceProvider();
+    // Setup our DI
+    var serviceProvider = new ServiceCollection()
+                              .AddLogging()
+                              .AddSingleton<IConfiguration>(config)
+                              .RegisterServices(config, args)
+                              .AddLogging((builder) => {
+                                builder.AddConsole();
+                                builder.SetMinimumLevel(LogLevel.Trace);
+                              })
+                              .BuildServiceProvider();
 
-        var updateService = serviceProvider.GetService<IDynDnsService>();
-        var ip = updateService.UpdateDynamicDnsRecord().GetAwaiter().GetResult();
-        Console.WriteLine($"Assigned IP {ip}");
-    }
+    var updateService = serviceProvider.GetService<IDynDnsService>();
+    var ip = updateService.UpdateDynamicDnsRecord().GetAwaiter().GetResult();
+    Console.WriteLine($"Assigned IP {ip}");
+  }
 }
 }
--- src/AzureDynDns/Services/IpFromArguments/IpFromArgumentsConfiguration.cs
+++ src/AzureDynDns/Services/IpFromArguments/IpFromArgumentsConfiguration.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace AzureDynDns.Services.IpFromArguments {
-public class IpFromArgumentsConfiguration {
+  public class IpFromArgumentsConfiguration {
     /// <summary>
     /// The index of the argument to use as the IP.
     /// </summary>
     public int ArgumentIndex {
-        get;
-        set;
+      get;
+      set;
     }
 
     /// <summary>
     /// The arguments passed to the application.
     /// </summary>
     public IList<string>Arguments {
-        get;
-        set;
+      get;
+      set;
     }
-}
+  }
 }
