40f3174d2e9731f60414513194b6bb783434bba5
Restyled.io <commits@restyled.io>
2020-06-06 14:14:40 +0000

    Restyled by astyle

--- src/AzureDynDns/Services/IpFromArguments/IpFromArgumentsService.cs
+++ src/AzureDynDns/Services/IpFromArguments/IpFromArgumentsService.cs
@@ -7,30 +7,30 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace AzureDynDns.Services.IpFromArguments {
-  public class IpFromArgumentsService : IIpProvider {
+public class IpFromArgumentsService : IIpProvider {
     private readonly ILogger<IIpProvider>logger;
     private IpFromArgumentsConfiguration config;
 
     public IpFromArgumentsService(IpFromArgumentsConfiguration config,
                                   ILogger<IIpProvider>logger) {
-      this.config = config;
-      this.logger = logger;
+        this.config = config;
+        this.logger = logger;
     }
 
     public Task<string>GetIP() {
-      if (config.ArgumentIndex < 0) {
-        throw new NegativeIndexException();
-      }
+        if (config.ArgumentIndex < 0) {
+            throw new NegativeIndexException();
+        }
 
-      if (config.Arguments == null || !config.Arguments.Any()) {
-        throw new NoArgumentsException();
-      }
+        if (config.Arguments == null || !config.Arguments.Any()) {
+            throw new NoArgumentsException();
+        }
 
-      if (config.ArgumentIndex > config.Arguments.Count - 1) {
-        throw new IndexOutOfBoundsException(config);
-      }
+        if (config.ArgumentIndex > config.Arguments.Count - 1) {
+            throw new IndexOutOfBoundsException(config);
+        }
 
-      return Task.FromResult(config.Arguments[config.ArgumentIndex]);
+        return Task.FromResult(config.Arguments[config.ArgumentIndex]);
     }
-  }
+}
 }
