e92b0df466573651b37b78684ea3cdffc90fdb54
Restyled.io <commits@restyled.io>
2020-06-06 11:17:12 +0000

    Restyled by clang-format

--- src/AzureDynDns/Services/IpFromArguments/IpFromArgumentsConfiguration.cs
+++ src/AzureDynDns/Services/IpFromArguments/IpFromArgumentsConfiguration.cs
@@ -2,24 +2,22 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace AzureDynDns.Services.IpFromArguments
-{
-public class IpFromArgumentsConfiguration
-{
+namespace AzureDynDns.Services.IpFromArguments {
+  public class IpFromArgumentsConfiguration {
     /// <summary>
     /// The index of the argument to use as the IP.
     /// </summary>
     public int ArgumentIndex {
-        get;
-        set;
+      get;
+      set;
     }
 
     /// <summary>
     /// The arguments passed to the application.
     /// </summary>
-    public IList<string> Arguments {
-        get;
-        set;
+    public IList<string>Arguments {
+      get;
+      set;
     }
-}
+  }
 }
