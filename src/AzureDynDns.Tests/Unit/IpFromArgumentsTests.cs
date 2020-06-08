6d0a4718a2795a89d419cb9daef3d38155e1b7fa
Restyled.io <commits@restyled.io>
2020-06-08 21:42:25 +0000

    Restyled by clang-format

--- src/AzureDynDns.Tests/Unit/IpFromArgumentsTests.cs
+++ src/AzureDynDns.Tests/Unit/IpFromArgumentsTests.cs
@@ -11,111 +11,89 @@ using Microsoft.Extensions.Logging;
 using Moq;
 using Xunit;
 
-namespace AzureDynDNS.Tests.Unit
-{
-public class IpFromArgumentsTests
-{
-    private static readonly string[] ArgumentsList = new string[] {
-        "device name", "1500", "1531", "192.168.1.2", "255.255.255.0"
-    };
-    private readonly ILogger<IIpProvider> logger;
+namespace AzureDynDNS.Tests.Unit {
+  public class IpFromArgumentsTests {
+    private static readonly string[] ArgumentsList = new string[]{
+        "device name", "1500", "1531", "192.168.1.2", "255.255.255.0"};
+    private readonly ILogger<IIpProvider>logger;
 
-    public IpFromArgumentsTests()
-    {
-        logger = new Mock<ILogger<IIpProvider>>().Object;
+    public IpFromArgumentsTests() {
+      logger = new Mock<ILogger<IIpProvider>>().Object;
     }
 
     [Theory]
     [MemberData(nameof(ArgumentsData))]
-    // Test that the service returns the right value for the given index
-    public async Task
-    Returns_correct_argument(int index, string expected_value)
-    {
-        // Prepare - The service
-        var config = new IpFromArgumentsConfiguration()
-        {
-            Arguments = ArgumentsList,
-            ArgumentIndex = index
-        };
-        var service = new IpFromArgumentsService(config, logger);
+        // Test that the service returns the right value for the given index
+        public async Task
+        Returns_correct_argument(int index, string expected_value) {
+      // Prepare - The service
+      var config = new IpFromArgumentsConfiguration(){Arguments = ArgumentsList,
+                                                      ArgumentIndex = index};
+      var service = new IpFromArgumentsService(config, logger);
 
-        // Act - Get the IP
-        var ip = await service.GetIP();
+      // Act - Get the IP
+      var ip = await service.GetIP();
 
-        // Assert
-        Assert.Equal(expected_value, ip);
+      // Assert
+      Assert.Equal(expected_value, ip);
     }
 
-    public static IEnumerable<object[]> ArgumentsData()
-    {
-        var output = new List<object[]>();
+    public static IEnumerable<object[]>ArgumentsData() {
+      var output = new List<object[]>();
 
-        for (var index = 0; index < ArgumentsList.Length; index++)
-        {
-            output.Add(new object[] { index, ArgumentsList[index] });
-        }
+      for (var index = 0; index < ArgumentsList.Length; index++) {
+        output.Add(new object[]{index, ArgumentsList[index]});
+      }
 
-        return output;
+      return output;
     }
 
     [Fact]
     public async Task
-    Throws_exceptions_if_no_arguments_passedAsync()
-    {
-        // Prepare - The service
-        var config = new IpFromArgumentsConfiguration()
-        {
-            Arguments = null,
-            ArgumentIndex = 0
-        };
-        var service = new IpFromArgumentsService(config, logger);
+    Throws_exceptions_if_no_arguments_passedAsync() {
+      // Prepare - The service
+      var config = new IpFromArgumentsConfiguration(){Arguments = null,
+                                                      ArgumentIndex = 0};
+      var service = new IpFromArgumentsService(config, logger);
 
-        // act
-        Func<Task> act = async () => await service.GetIP();
+      // act
+      Func<Task>act = async () => await service.GetIP();
 
-        // assert
-        var exception = await Assert.ThrowsAsync<NoArgumentsException>(act);
+      // assert
+      var exception = await Assert.ThrowsAsync<NoArgumentsException>(act);
     }
 
     [Fact]
     public async Task
-    Throws_exceptions_on_negative_argument_indexAsync()
-    {
-        // Prepare - The service
-        var config = new IpFromArgumentsConfiguration()
-        {
-            Arguments = null,
-            ArgumentIndex = -1
-        };
-        var service = new IpFromArgumentsService(config, logger);
+    Throws_exceptions_on_negative_argument_indexAsync() {
+      // Prepare - The service
+      var config = new IpFromArgumentsConfiguration(){Arguments = null,
+                                                      ArgumentIndex = -1};
+      var service = new IpFromArgumentsService(config, logger);
 
-        // act
-        Func<Task> act = async () => await service.GetIP();
+      // act
+      Func<Task>act = async () => await service.GetIP();
 
-        // assert
-        var exception = await Assert.ThrowsAsync<NegativeIndexException>(act);
+      // assert
+      var exception = await Assert.ThrowsAsync<NegativeIndexException>(act);
     }
 
     [Fact]
     public async Task
-    Throws_exceptions_on_out_of_bounds_argument_indexAsync()
-    {
-        // Prepare - The service
-        var config = new IpFromArgumentsConfiguration()
-        {
-            Arguments = ArgumentsList,
-            ArgumentIndex = ArgumentsList.Count()
-        };
-        var service = new IpFromArgumentsService(config, logger);
+    Throws_exceptions_on_out_of_bounds_argument_indexAsync() {
+      // Prepare - The service
+      var config = new IpFromArgumentsConfiguration(){
+          Arguments = ArgumentsList, ArgumentIndex = ArgumentsList.Count()};
+      var service = new IpFromArgumentsService(config, logger);
 
-        // act
-        Func<Task> act = async () => await service.GetIP();
+      // act
+      Func<Task>act = async () => await service.GetIP();
 
-        // assert
-        var exception = await Assert.ThrowsAsync<IndexOutOfBoundsException>(act);
-        Assert.NotNull(exception);
-        Assert.Equal(config.Arguments.Count, exception.ElementsCount);
-        Assert.Equal(config.ArgumentIndex, exception.RequestedIndex);
+      // assert
+      var exception = await Assert.ThrowsAsync<IndexOutOfBoundsException>(act);
+      Assert.NotNull(exception);
+      Assert.Equal(config.Arguments.Count, exception.ElementsCount);
+      Assert.Equal(config.ArgumentIndex, exception.RequestedIndex);
     }
-}
+  }
 }
--- src/AzureDynDns/Services/IpFromArguments/Exceptions/NegativeIndexException.cs
+++ src/AzureDynDns/Services/IpFromArguments/Exceptions/NegativeIndexException.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 
-namespace AzureDynDns.Services.IpFromArguments.Exceptions
-{
-public class NegativeIndexException : Exception
-{
+namespace AzureDynDns.Services.IpFromArguments.Exceptions {
+  public class NegativeIndexException : Exception {
     public NegativeIndexException()
-        : base("ArgumentIndex: Invalid index. Less than zero.")
-    {
-    }
-}
+        : base("ArgumentIndex: Invalid index. Less than zero.") {}
+  }
 }
